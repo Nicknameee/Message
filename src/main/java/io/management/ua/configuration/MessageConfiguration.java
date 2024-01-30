99aa1df30e7161999c9c9c5f5bb47336ce15fc63
Nicknameee <igrunka1991@gmail.com>
2024-01-31 01:06:30 +0200

    MailSenderBean

--- src/main/java/io/management/ua/configuration/MessageConfiguration.java
+++ src/main/java/io/management/ua/configuration/MessageConfiguration.java
@@ -1,5 +1,6 @@
 package io.management.ua.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -7,8 +8,51 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MessageConfiguration {
+    @Value("${spring.mail.default-encoding}")
+    private String defaultEncoding;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean startTlsEnable;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.required}")
+    private boolean startTlsRequired;
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean smtpAuth;
+
+    @Value("${spring.mail.properties.mail.smtp.ssl.trust}")
+    private String sslTrust;
+
     @Bean
     public JavaMailSender javaMailSender() {
-        return new JavaMailSenderImpl();
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setDefaultEncoding(defaultEncoding);
+        javaMailSender.setHost(host);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        javaMailSender.setPort(port);
+        javaMailSender.setProtocol(protocol);
+
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.starttls.enable", String.valueOf(startTlsEnable));
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.starttls.required", String.valueOf(startTlsRequired));
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.auth", String.valueOf(smtpAuth));
+        javaMailSender.getJavaMailProperties().setProperty("mail.smtp.ssl.trust", sslTrust);
+
+        return javaMailSender;
     }
 }
--- src/main/java/io/management/ua/mails/service/MailService.java
+++ src/main/java/io/management/ua/mails/service/MailService.java
@@ -1,17 +1,21 @@
 package io.management.ua.mails.service;
 
 import io.management.ua.amqp.messages.MessageModel;
+import io.management.ua.exceptions.ActionRestrictedException;
 import io.management.ua.mails.entity.Mail;
 import io.management.ua.mails.mappers.MailMapper;
 import io.management.ua.mails.repository.MailRepository;
 import io.management.ua.messages.MessageService;
 import io.management.ua.utility.TimeUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -21,15 +25,27 @@ public class MailService implements MessageService {
     private final MailRepository mailRepository;
     private final JavaMailSender javaMailSender;
     private final MailMapper mailMapper;
-    @Value("${spring.mail.username}")
-    private String source;
+    private final Environment environment;
 
     @Override
     public void processMessage(MessageModel messageModel) {
         Mail mail = mailMapper.messageModelToMail(messageModel);
         mail.setSendingDate(TimeUtil.getCurrentDateTime());
+        mail.setSender(environment.getProperty("spring.mail.username"));
 
-        javaMailSender.send(getMailMessage(mail));
+        switch (messageModel.getMessageType()) {
+            case PLAIN_TEXT ->
+                    javaMailSender.send(getMailMessage(mail));
+            case HTML -> {
+                try {
+                    javaMailSender.send(getHtmlMailMessage(mail));
+                } catch (MessagingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            case WITH_FILE -> throw new ActionRestrictedException("Media files currently not supported");
+            default -> throw new ActionRestrictedException("Unknown message type or type was not specified");
+        }
 
         mailRepository.save(mail);
     }
@@ -37,12 +53,23 @@ public class MailService implements MessageService {
     private SimpleMailMessage getMailMessage(Mail mail) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setSentDate(TimeUtil.getCurrentDate());
-        simpleMailMessage.setFrom(source);
+        simpleMailMessage.setFrom(mail.getSender());
         simpleMailMessage.setTo(mail.getReceiver());
         simpleMailMessage.setText(mail.getContent());
         simpleMailMessage.setSubject(mail.getSubject());
 
         return simpleMailMessage;
+    }
+
+    private MimeMessage getHtmlMailMessage(Mail mail) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
+        helper.setFrom(mail.getSender());
+        helper.setTo(mail.getReceiver());
+        helper.setSubject(mail.getSubject());
+        helper.setText(mail.getContent(), true);
+
+        return mimeMessage;
     }
 
     @Override
